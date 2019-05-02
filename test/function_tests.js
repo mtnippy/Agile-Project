f1873767dc286f2352cb2f9fcf467c3f6704e484
mtnippy <32887729+mtnippy@users.noreply.github.com>
2019-05-02 15:16:58 -0700

    Unit testing - Semi-Working

--- test/function_tests.js
+++ test/function_tests.js
@@ -2,6 +2,20 @@ const assert = require('chai').assert;
 const fightingSaves = require('../javascript/fighting_saves.js');
 const userDB = require('../javascript/user_db')
 const app = require('../app.js');
+const chai = require('chai');
+chai.use(require('chai-http'));
+const expect = require('chai').expect;
+
+const firebase = require('firebase');
+var firebaseConfig = {
+    apiKey: "AIzaSyCJorziWo3lcBRSxEqaTD-WMFEP-0VxqOY",
+    authDomain: "test-6ec85.firebaseapp.com",
+    databaseURL: "https://test-6ec85.firebaseio.com",
+    projectId: "test-6ec85",
+    storageBucket: "test-6ec85.appspot.com",
+    messagingSenderId: "483947477248"
+};
+firebase.initializeApp(firebaseConfig);
 
 describe('adding', function(){
     // Unit test "it" block for adding info upon character creation
@@ -17,6 +31,14 @@ describe('adding', function(){
 describe('creating', function(){
     it('Creating user, expect good fields', function(){
         var actual = userDB.add_new_user('Andy', 'Boi', 'happy@gspot.com', 'doo', 'doo');
+        assert.equal(actual, 'Your account is created!');
+    });
+});
+
+
+describe('creating', function(){
+    it('Creating user, expect good fields', function(){
+        var actual = userDB.email_get('happy@gspot.com');
         assert.equal(actual, 'Your account is created!');
     });
 });
